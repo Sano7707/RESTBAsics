8c07ba92ba0598431cb611ca6d8954fe73d6fa72
Simone Avancini <simone.avancini@studenti.unitn.it>
2024-11-02 18:18:11 +0100

    Updated stuff

--- examples/routes/frameworkRoutes.js
+++ examples/routes/frameworkRoutes.js
@@ -1,17 +1,67 @@
 const express = require('express');
 const router = express.Router();
 
-const {
-    getAllFrameworks,
-    addComment,
-    updateLink
-} = require('../controllers/frameworkController');
+router.get('/', async function(req, res) {
+    if (Object.keys(req.query).length > 0) {
+        return res.status(404).json({ message: 'Not Found' });
+    }
 
-router.get('/', getAllFrameworks);
+    try {
+        const frameworks = await Framework.find();
+        res.json(frameworks);
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+});
 
-router.post('/:frameworkId/comments', addComment);
+router.post('/:frameworkId/comments', async function(req, res) {
+    const { frameworkId } = req.params;
+    const { user, comment } = req.body;
 
-router.put('/:id', updateLink);
+    try {
+        const framework = await Framework.findById(frameworkId);
+        if (!framework) {
+            return res.status(404).json({ message: 'Framework not found' });
+        }
+
+        const tutorial = framework.tutorial;
+
+        if (!tutorial) {
+            return res.status(404).json({ message: 'Tutorial not found' });
+        }
+
+        tutorial.comments.push({ user, comment, date: new Date() });
+
+        await framework.save();
+
+        res.status(201).json(framework);
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+});
+
+router.put('/:id', async (req, res) => {
+    try {
+        const framework = await Framework.findById(req.params.id);
+        if (!framework) {
+            return res.status(404).json({ message: 'Framework not found' });
+        }
+
+        if (req.body.documentationLink) {
+            framework.documentationLink = req.body.documentationLink;
+        }
+
+        if (req.body.tutorialLink) {
+            framework.tutorial.tutorialLink = req.body.tutorialLink;
+        }
+
+        const updatedFramework = await framework.save();
+
+        res.json({ message: 'Links updated successfully', framework: updatedFramework });
+    } catch (err) {
+        res.status(400).json({ message: err.message });
+    }
+});
 
 router.all('*', (req, res) => {
     res.status(404).json({ message: 'Not Found' });
--- examples/routes/programmingLanguageRoutes.js
+++ examples/routes/programmingLanguageRoutes.js
@@ -1,13 +1,30 @@
 const express = require('express');
 const router = express.Router();
-const {
-    getAllLanguages,
-    getLanguageById
-} = require('../controllers/programmingLanguageController');
 
-router.get('/', getAllLanguages);
+router.get('/', async function(req, res) {
+    if (Object.keys(req.query).length > 0) {
+        return res.status(404).json({ message: 'Not Found' });
+    }
 
-router.get('/:name', getLanguageById);
+    try {
+        const languages = await ProgrammingLanguage.find();
+        res.json(languages);
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+});
+
+router.get('/:name', async function(req, res) {
+    try {
+        const language = await ProgrammingLanguage.findOne({ name: req.params.name });
+        if (!language) {
+            return res.status(404).json({ message: 'Programming language not found' });
+        }
+        res.json(language);
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+});
 
 router.all('*', (req, res) => {
     res.status(404).json({ message: 'Not Found' });
